fcb67752b56ea8c307680ae9ff846d5cf9042b0a
Donstesh <stephenshifoko@gmail.com>
2020-11-02 16:01:18 +0300

    register models

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Product)
+admin.site.register(Sales)
+admin.site.register(Supllies)
+admin.site.register(Expenditure)
+admin.site.register(Employee)
