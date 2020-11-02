473bea7309ea3906b5f5517a5a258c11f604db5e
Donstesh <stephenshifoko@gmail.com>
2020-11-02 16:01:41 +0300

    forms

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,8 +1,38 @@
 from django import forms
 from .models import Product
+from .models import Sales
+from .models import Supllies
+from .models import Expenditure
+from .models import Employee
+
 
 class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
         fields = ('name', 'cetagory', 'supplier', 'unit_price', 'description')
+
+class SuplliesForm(forms.ModelForm):
+
+    class Meta:
+        model = Supllies
+        fields = ('details', 'supplier', 'quanitity', 'date', 'balance', 'checkedin')
+
+class SalesForm(forms.ModelForm):
+
+    class Meta:
+        model = Sales
+        fields = ('details', 'price', 'quantity', 'date', 'sold_at')
+
+
+class ExpenditureForm(forms.ModelForm):
+
+    class Meta:
+        model = Expenditure
+        fields = ('details', 'amount_requisitioned', 'date', 'requisitioned_by')
+
+class EmployeeForm(forms.ModelForm):
+
+    class Meta:
+        model = Employee
+        fields = ('emp_name', 'emp_email', 'emp_ID', 'date_of_employement', 'designation', 'salary', 'health_cert_no')
